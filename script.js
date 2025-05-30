b3bc666b13a20fc32be37249a71ac5dd35dea2ec
ram <ramgoennka08@gmail.com>
2025-05-30 01:25:24 -0500

    style changes

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('light-mode');
-    var themeToggleText = document.body.classList.contains('light-mode') ? '🌞' : '🌜';
-    document.getElementById('theme-toggle').textContent = themeToggleText;
-});
